1f84b527262bb82ec162ac374fd37d7609c83cc0
Atnanasi <ys2000pro@gmail.com>
2016-09-29 22:55:03 +0900

    Add main.py ThinkPadIsSaikou

---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: UTF-8 -*-
+
+#thinkpad-is-saikou
+#Copyright (c) 2016 Atnanasi
+#Released under the MIT license
+#https://github.com/Atnanasi/thinkpad-is-saikou/blob/master/LICENSE
+
+def ThinkpadIs():
+    return "Saikou"
+
+if __name__=="__main__":
+    print("Thinkpad is",ThinkpadIs())
